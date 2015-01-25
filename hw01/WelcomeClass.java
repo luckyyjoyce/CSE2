2e54de2d1c61dcdf6849fb87d57b0c17c096a270
luckyyjoyce <rul218@lehigh.edu>
2015-01-25 03:54:13 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+/////////////////////////////////////////
+//Ruoting Li
+//January 24th, 2015
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+  public static void main(String[ ] args) {
+      
+//  print the statement with box around it
+    System.out.println("  -----------  ");
+    System.out.println("  | WELCOME |  ");
+    System.out.println("  -----------  ");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-R--U--L--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("Last August is my first time to be in the USA. I was so excited about the beautiful campus");
+
+    //test/testtest
+  }
+}
